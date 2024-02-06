96bca1ce94a84476bb66573f320e209fbbdc7aec
Matthew Conrad <conrad.matthewj@gmail.com>
2024-02-05 20:04:34 -0500

    Initial Commit

---
+++ PKHeXMac/PKHeXMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  PKHeXMacApp.swift
+//  PKHeXMac
+//
+//  Created by Matthew Conrad on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PKHeXMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
